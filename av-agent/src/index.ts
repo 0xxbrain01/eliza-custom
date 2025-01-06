f60196a5df075bb3d5c5695ebfed70517aa06f43
0xxbrain01 <0xxbrain@gmail.com>
2025-01-06 11:17:32 +0700

    add

--- av-agent/src/index.ts
+++ av-agent/src/index.ts
@@ -3,6 +3,8 @@ import {settings,
     IDatabaseCacheAdapter} from '@elizaos/core';
 import path from "path";
 import { fileURLToPath } from "url";
+import { SqliteDatabaseAdapter } from "@elizaos/adapter-sqlite";
+import Database from "better-sqlite3";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename);
@@ -16,11 +18,15 @@ const startAgent = async () => {
     const dataDir = path.join(__dirname, "../data");
     console.log("🚀 ~ startAgent ~ dataDir:", dataDir)
 
-
+    db = initializeDatabase(dataDir);
+    await db.init();
 }
 
 function initializeDatabase(dataDir: string) {
-  // save database on 
+  // save database on
+  const filePath = path.join(dataDir, "db.sqlite");
+  const db = new SqliteDatabaseAdapter(new Database(filePath));
+  return db;
 }
 
 
